40244924c2c4ee1df0163a551dba82f72141bc1d
miconakase17 <miconakaseyt@gmail.com>
2025-09-11 10:51:16 +0800

    Update ReservationSystem.sql

--- script/ReservationSystem.sql
+++ script/ReservationSystem.sql
@@ -1,272 +1,278 @@
--- phpMyAdmin SQL Dump
--- version 5.2.1
--- https://www.phpmyadmin.net/
+-- MySQL dump 10.13  Distrib 8.0.42, for Win64 (x86_64)
 --
--- Host: 127.0.0.1
--- Generation Time: Sep 04, 2025 at 05:31 PM
--- Server version: 10.4.32-MariaDB
--- PHP Version: 8.2.12
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: localhost    Database: reservation_system
+-- ------------------------------------------------------
+-- Server version	9.2.0
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `reservation_system`
---
-
--- --------------------------------------------------------
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `messages`
 --
 
+DROP TABLE IF EXISTS `messages`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `messages` (
-  `messageID` int(11) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `message` text NOT NULL,
-  `createdAt` datetime DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `messageID` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `email` varchar(100) COLLATE utf8mb4_general_ci NOT NULL,
+  `message` text COLLATE utf8mb4_general_ci NOT NULL,
+  `createdAt` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`messageID`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `messages`
 --
 
-INSERT INTO `messages` (`messageID`, `name`, `email`, `message`, `createdAt`) VALUES
-(1, 'Mhyca Varona', 'mhycavarona@yahoo.com', 'Hi beb', '2025-05-28 06:10:17'),
-(2, 'Julie AnAn', 'juliemabangisxz@yahoo.com', 'Pwede ka ba sapakin?', '2025-05-28 06:11:38'),
-(3, 'Arvin Bananini', 'arvintusta@gmail.com', 'I love Valerina Cappuccino', '2025-05-28 06:13:28'),
-(4, 'Eriz Pisot', 'erizpisot@gmail.com', 'I love dinosaurs', '2025-05-28 06:21:14');
-
--- --------------------------------------------------------
+LOCK TABLES `messages` WRITE;
+/*!40000 ALTER TABLE `messages` DISABLE KEYS */;
+INSERT INTO `messages` VALUES (1,'Mhyca Varona','mhycavarona@yahoo.com','Hi beb','2025-05-28 06:10:17'),(2,'Julie AnAn','juliemabangisxz@yahoo.com','Pwede ka ba sapakin?','2025-05-28 06:11:38'),(3,'Arvin Bananini','arvintusta@gmail.com','I love Valerina Cappuccino','2025-05-28 06:13:28'),(4,'Eriz Pisot','erizpisot@gmail.com','I love dinosaurs','2025-05-28 06:21:14');
+/*!40000 ALTER TABLE `messages` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `payments`
 --
 
+DROP TABLE IF EXISTS `payments`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `payments` (
-  `paymentID` int(11) NOT NULL,
-  `userID` int(11) NOT NULL,
-  `reservationID` int(11) NOT NULL,
+  `paymentID` int NOT NULL AUTO_INCREMENT,
+  `userID` int NOT NULL,
+  `reservationID` int NOT NULL,
   `amount` decimal(10,2) NOT NULL,
   `paymentDate` datetime NOT NULL,
-  `paymentMethod` varchar(50) NOT NULL,
-  `paymentStatus` varchar(50) NOT NULL,
-  `transactionReference` varchar(100) DEFAULT NULL,
-  `createdAt` datetime DEFAULT current_timestamp(),
-  `lastUpdate` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp()
+  `paymentMethod` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `paymentStatus` varchar(50) COLLATE utf8mb4_general_ci NOT NULL,
+  `transactionReference` varchar(100) COLLATE utf8mb4_general_ci DEFAULT NULL,
+  `createdAt` datetime DEFAULT CURRENT_TIMESTAMP,
+  `lastUpdate` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`paymentID`),
+  KEY `userID` (`userID`),
+  KEY `reservationID` (`reservationID`),
+  CONSTRAINT `fk_payments_reservations` FOREIGN KEY (`reservationID`) REFERENCES `reservations` (`reservationID`),
+  CONSTRAINT `fk_payments_users` FOREIGN KEY (`userID`) REFERENCES `users` (`userID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `payments`
+--
+
+LOCK TABLES `payments` WRITE;
+/*!40000 ALTER TABLE `payments` DISABLE KEYS */;
+/*!40000 ALTER TABLE `payments` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `reservations`
 --
 
+DROP TABLE IF EXISTS `reservations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `reservations` (
-  `reservationID` int(11) NOT NULL,
-  `userID` int(11) NOT NULL,
-  `serviceID` int(11) NOT NULL,
+  `reservationID` int NOT NULL AUTO_INCREMENT,
+  `userID` int NOT NULL,
+  `serviceID` int NOT NULL,
   `date` date NOT NULL,
   `startTime` time NOT NULL,
   `endTime` time NOT NULL,
   `totalCost` decimal(10,2) NOT NULL,
-  `statusID` int(11) NOT NULL,
-  `createdAt` datetime DEFAULT current_timestamp()
+  `statusID` int NOT NULL,
+  `createdAt` datetime DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`reservationID`),
+  KEY `userID` (`userID`),
+  KEY `serviceID` (`serviceID`),
+  KEY `statusID` (`statusID`),
+  CONSTRAINT `fk_reservations_roles` FOREIGN KEY (`serviceID`) REFERENCES `services` (`serviceID`),
+  CONSTRAINT `fk_reservations_status` FOREIGN KEY (`statusID`) REFERENCES `status` (`statusID`),
+  CONSTRAINT `fk_reservations_users` FOREIGN KEY (`userID`) REFERENCES `users` (`userID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- --------------------------------------------------------
+--
+-- Dumping data for table `reservations`
+--
+
+LOCK TABLES `reservations` WRITE;
+/*!40000 ALTER TABLE `reservations` DISABLE KEYS */;
+/*!40000 ALTER TABLE `reservations` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `roles`
 --
 
+DROP TABLE IF EXISTS `roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `roles` (
-  `roleID` int(11) NOT NULL,
-  `roleName` varchar(50) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `roleID` int NOT NULL AUTO_INCREMENT,
+  `roleName` varchar(45) COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`roleID`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `roles`
 --
 
-INSERT INTO `roles` (`roleID`, `roleName`) VALUES
-(1, 'Admin'),
-(2, 'Customer');
+LOCK TABLES `roles` WRITE;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+INSERT INTO `roles` VALUES (1,'0'),(2,'0');
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- --------------------------------------------------------
+--
+-- Table structure for table `services`
+--
+
+DROP TABLE IF EXISTS `services`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `services` (
+  `serviceID` int NOT NULL,
+  `serviceName` varchar(45) NOT NULL,
+  PRIMARY KEY (`serviceID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `services`
+--
+
+LOCK TABLES `services` WRITE;
+/*!40000 ALTER TABLE `services` DISABLE KEYS */;
+/*!40000 ALTER TABLE `services` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `status`
 --
 
+DROP TABLE IF EXISTS `status`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `status` (
-  `statusID` int(11) NOT NULL,
-  `statusName` varchar(45) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `statusID` int NOT NULL AUTO_INCREMENT,
+  `statusName` varchar(45) COLLATE utf8mb4_general_ci NOT NULL,
+  PRIMARY KEY (`statusID`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `status`
 --
 
-INSERT INTO `status` (`statusID`, `statusName`) VALUES
-(1, 'Active'),
-(2, 'Inactive');
+LOCK TABLES `status` WRITE;
+/*!40000 ALTER TABLE `status` DISABLE KEYS */;
+INSERT INTO `status` VALUES (1,'Active'),(2,'Inactive');
+/*!40000 ALTER TABLE `status` ENABLE KEYS */;
+UNLOCK TABLES;
 
--- --------------------------------------------------------
+--
+-- Table structure for table `user_details`
+--
+
+DROP TABLE IF EXISTS `user_details`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_details` (
+  `userID` int NOT NULL,
+  `lastName` varchar(45) NOT NULL,
+  `firstName` varchar(45) NOT NULL,
+  `middleName` varchar(45) DEFAULT NULL,
+  `phoneNumber` varchar(45) NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `lastUpdate` varchar(45) NOT NULL,
+  KEY `fk_user_details_users` (`userID`),
+  CONSTRAINT `fk_user_details_users` FOREIGN KEY (`userID`) REFERENCES `users` (`userID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_details`
+--
+
+LOCK TABLES `user_details` WRITE;
+/*!40000 ALTER TABLE `user_details` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_details` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_roles`
+--
+
+DROP TABLE IF EXISTS `user_roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_roles` (
+  `userID` int NOT NULL,
+  `roleID` int NOT NULL,
+  KEY `fk_user_roles_roles` (`roleID`),
+  KEY `fk_user_roles_users` (`userID`),
+  CONSTRAINT `fk_user_roles_roles` FOREIGN KEY (`roleID`) REFERENCES `roles` (`roleID`),
+  CONSTRAINT `fk_user_roles_users` FOREIGN KEY (`userID`) REFERENCES `users` (`userID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_roles`
+--
+
+LOCK TABLES `user_roles` WRITE;
+/*!40000 ALTER TABLE `user_roles` DISABLE KEYS */;
+/*!40000 ALTER TABLE `user_roles` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `users`
 --
 
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `users` (
-  `userID` int(11) NOT NULL,
-  `lastName` varchar(45) NOT NULL,
-  `firstName` varchar(45) NOT NULL,
-  `middleName` varchar(45) NOT NULL,
-  `username` varchar(45) NOT NULL,
-  `phoneNumber` varchar(15) NOT NULL,
-  `email` varchar(100) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `roleID` int(11) DEFAULT NULL,
-  `statusID` int(11) DEFAULT NULL,
-  `createdAt` datetime DEFAULT current_timestamp(),
-  `lastUpdate` datetime DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-  `isActive` tinyint(4) DEFAULT 1
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+  `userID` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) COLLATE utf8mb4_general_ci NOT NULL,
+  `password` varchar(255) COLLATE utf8mb4_general_ci NOT NULL,
+  `createdAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `isActive` tinyint NOT NULL DEFAULT '1',
+  PRIMARY KEY (`userID`)
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`userID`, `lastName`, `firstName`, `middleName`, `username`, `phoneNumber`, `email`, `password`, `roleID`, `statusID`, `createdAt`, `lastUpdate`, `isActive`) VALUES
-(8, 'Nakase', 'Mico', 'Libunao', 'miconakase17', '09665270518', 'miconakaseyt@gmail.com', '$2y$10$3iZBCAVb.utS5WSjL2OrUu6QEQhtCXG45Csuo9rrtYsfqkTVqJwZK', NULL, NULL, '2025-05-20 09:12:28', NULL, NULL),
-(9, 'Gavino', 'John Rey', 'N/A', 'jepoy69', '09998876654', 'jepoy69@gmail.com', '$2y$10$dJWhai6vP.kSTh2RyNC8M.8HCxqYTePxB2LMN5UfSzGKi1CiS0jqK', NULL, NULL, '2025-05-20 09:13:52', NULL, NULL),
-(11, 'Varona', 'Mhyca', 'Gacilo', 'Mhics25', '09519614271', 'varonamhics@gmail.com', '$2y$10$SYKNe6Y81ZhN5Q6VBhHqneKcAAdPn94njXCJyLProet1WMdXRIf2a', NULL, NULL, '2025-05-26 05:25:48', '2025-05-26 05:25:48', NULL),
-(12, 'Arandia', 'Jim', '', 'jim123', '09767889876', 'jim123@yahoo.com', '$2y$10$wtCbrHV9LpyJStlPfCVQtu6358OaiDJY3CuDJxt/p/X1MQQgFcBbe', NULL, NULL, '2025-05-29 05:49:40', '2025-05-29 05:49:40', NULL),
-(13, 'Carpio', 'Julie Ann', 'Bonguit', 'julieanncarpio', '09675568887', 'julieanncarpio55@gmail.com', '$2y$10$3XT7KUh8z3TwNc245EtZMOAAYEJARVV79X9U5nawS/J.Ify1aNozm', NULL, NULL, '2025-08-26 08:15:28', '2025-08-26 08:15:28', NULL),
-(14, 'Pandaan', 'Sally', '', 'sallymae', '09876779988', 'truchorus.official@gmail.com', '$2y$10$O3jcU9/JhpBJi00vqhBwueiZXxPICL1UGa/Elrc6kxV.Kdo0DiqKi', NULL, NULL, '2025-09-03 11:40:36', '2025-09-03 11:40:36', NULL);
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (8,'miconakase17','$2y$10$3iZBCAVb.utS5WSjL2OrUu6QEQhtCXG45Csuo9rrtYsfqkTVqJwZK','2025-05-20 09:12:28',0),(9,'jepoy69','$2y$10$dJWhai6vP.kSTh2RyNC8M.8HCxqYTePxB2LMN5UfSzGKi1CiS0jqK','2025-05-20 09:13:52',0),(11,'Mhics25','$2y$10$SYKNe6Y81ZhN5Q6VBhHqneKcAAdPn94njXCJyLProet1WMdXRIf2a','2025-05-26 05:25:48',0),(12,'jim123','$2y$10$wtCbrHV9LpyJStlPfCVQtu6358OaiDJY3CuDJxt/p/X1MQQgFcBbe','2025-05-29 05:49:40',0),(13,'julieanncarpio','$2y$10$3XT7KUh8z3TwNc245EtZMOAAYEJARVV79X9U5nawS/J.Ify1aNozm','2025-08-26 08:15:28',0),(14,'sallymae','$2y$10$O3jcU9/JhpBJi00vqhBwueiZXxPICL1UGa/Elrc6kxV.Kdo0DiqKi','2025-09-03 11:40:36',0);
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `messages`
---
-ALTER TABLE `messages`
-  ADD PRIMARY KEY (`messageID`);
-
---
--- Indexes for table `payments`
---
-ALTER TABLE `payments`
-  ADD PRIMARY KEY (`paymentID`),
-  ADD KEY `userID` (`userID`),
-  ADD KEY `reservationID` (`reservationID`);
-
---
--- Indexes for table `reservations`
---
-ALTER TABLE `reservations`
-  ADD PRIMARY KEY (`reservationID`),
-  ADD KEY `userID` (`userID`),
-  ADD KEY `serviceID` (`serviceID`),
-  ADD KEY `statusID` (`statusID`);
-
---
--- Indexes for table `roles`
---
-ALTER TABLE `roles`
-  ADD PRIMARY KEY (`roleID`);
-
---
--- Indexes for table `status`
---
-ALTER TABLE `status`
-  ADD PRIMARY KEY (`statusID`);
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`userID`),
-  ADD KEY `roleID` (`roleID`),
-  ADD KEY `statusID` (`statusID`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `messages`
---
-ALTER TABLE `messages`
-  MODIFY `messageID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
-
---
--- AUTO_INCREMENT for table `payments`
---
-ALTER TABLE `payments`
-  MODIFY `paymentID` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `reservations`
---
-ALTER TABLE `reservations`
-  MODIFY `reservationID` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `roles`
---
-ALTER TABLE `roles`
-  MODIFY `roleID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table `status`
---
-ALTER TABLE `status`
-  MODIFY `statusID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT for table `users`
---
-ALTER TABLE `users`
-  MODIFY `userID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
-
---
--- Constraints for dumped tables
---
-
---
--- Constraints for table `payments`
---
-ALTER TABLE `payments`
-  ADD CONSTRAINT `fk_payments_reservations` FOREIGN KEY (`reservationID`) REFERENCES `reservations` (`reservationID`),
-  ADD CONSTRAINT `fk_payments_users` FOREIGN KEY (`userID`) REFERENCES `users` (`userID`);
-
---
--- Constraints for table `reservations`
---
-ALTER TABLE `reservations`
-  ADD CONSTRAINT `fk_reservations_status` FOREIGN KEY (`statusID`) REFERENCES `status` (`statusID`),
-  ADD CONSTRAINT `fk_reservations_users` FOREIGN KEY (`userID`) REFERENCES `users` (`userID`);
-
---
--- Constraints for table `users`
---
-ALTER TABLE `users`
-  ADD CONSTRAINT `fk_users_roles` FOREIGN KEY (`roleID`) REFERENCES `roles` (`roleID`),
-  ADD CONSTRAINT `fk_users_status` FOREIGN KEY (`statusID`) REFERENCES `status` (`statusID`);
-COMMIT;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-09-11 10:49:13
