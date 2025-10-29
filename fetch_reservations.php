c3027ee253fcdc758c66f9584a4c7b631c86570f
Mico Nakase <miconakaseyt@gmail.com>
2025-10-29 15:02:25 +0800

    kjh

--- fetch_reservations.php
+++ fetch_reservations.php
@@ -1,6 +1,6 @@
 <?php
 header('Content-Type: application/json');
-require_once 'config/database.php'; // make sure this path is correct
+require_once 'config/database.php';
 
 // ✅ Get connection from Database class
 $conn = Database::getConnection();
